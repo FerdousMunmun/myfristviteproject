0aa29bdad999be6e0f5d68962107987c3ee0e9a0
FerdousMunmun <ferdousmunmun75@gmail.com>
2025-12-05 22:10:20 +0600

    add

--- src/components/Layouts/Banner.jsx
+++ src/components/Layouts/Banner.jsx
@@ -4,7 +4,7 @@ import Button from '../Button';
 
 const Banner = () => {
   return (
-    <div className="bg-[url('/src/assets/bannerbg.jpg')] py-[350px] bg-no-repeat bg-center bg-cover">
+    <div className="bg-[url('/src/assets/bannerbg.jpg')] py-[355px] bg-no-repeat bg-center bg-cover">
         <Heading className={"font-extrabold font-openSans leading-[70px] pr-[18px] text-[72px] w-[700px] text-[#192239 text-center]"} text={"We Are Digital Marketing Agency"} as={"h1"}/>
         
         <Heading className={"text-[20px] font-paprika leading-8 text-[#6C7D93] pt-5 w-[700px]"} text={"Use customer data to build great and solid product experiences that convert. Digital marketing’s development has changed the way brands and businesses use technology for marketing."} as={"h2"}/>
